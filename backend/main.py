4dcd4e562af44d17a9f1fe2722ac5a8e2c0f68d0
Justin W. Lin <justin@lin.ai>
2024-05-04 23:48:32 -0700

    feat: Add `/grounding_dino` endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,41 +1,88 @@
 import json
 import os
-from fastapi import FastAPI, UploadFile, HTTPException
+
+import torch
+from fastapi import FastAPI, HTTPException, UploadFile
 from fastapi.responses import FileResponse
+from PIL import Image
 from pydantic import BaseModel
-import uvicorn
+from transformers import AutoModelForZeroShotObjectDetection, AutoProcessor
+import decord
 
 FILE_PATH = "files"
 app = FastAPI()
 
+model_id = "IDEA-Research/grounding-dino-base"
+device = "cuda"
+processor = AutoProcessor.from_pretrained(model_id)
+model = AutoModelForZeroShotObjectDetection.from_pretrained(model_id).to(device)
+
+
 class VideoMetadata(BaseModel):
     json_data: dict
 
-@app.post("/drone_footage", response_model=VideoMetadata)
-async def upload_drone_footage(file: UploadFile) -> tuple[FileResponse, VideoMetadata]:
-    if file.filename and file.filename.endswith(".mp4"):
-        file_path = os.path.join(str(FILE_PATH), str(file.filename))
-        if os.path.exists(path=file_path):
-            processed_folder = file.filename.split('.')[0]
-            processed_video_path = os.path.join(FILE_PATH, processed_folder, file.filename)
-            json_file_path = os.path.join(FILE_PATH, processed_folder, f"{file.filename.split('.')[0]}.json")
-            
-            if os.path.exists(processed_video_path) and os.path.exists(json_file_path):
-                with open(json_file_path, "r") as json_file:
-                    json_data = json.load(json_file)
-                
-                return FileResponse(processed_video_path), VideoMetadata(json_data=json_data)
-            else:
-                raise HTTPException(status_code=404, detail="Processed video and metadata not found.")
-        else:
-            raise HTTPException(status_code=404, detail="Uploaded file not found.")
+
+def get_video_from_path(video_name: str) -> tuple[FileResponse, VideoMetadata]:
+    file_path = os.path.join(FILE_PATH, video_name)
+    if not os.path.exists(file_path):
+        raise HTTPException(status_code=404, detail="Video file not found.")
+
+    processed_folder = video_name.split(".")[0]
+    processed_video_path = os.path.join(FILE_PATH, processed_folder, video_name)
+    json_file_path = os.path.join(
+        FILE_PATH, processed_folder, f"{video_name.split('.')[0]}.json"
+    )
+
+    if os.path.exists(processed_video_path) and os.path.exists(json_file_path):
+        with open(json_file_path, "r") as json_file:
+            json_data = json.load(json_file)
+
+        return FileResponse(processed_video_path), VideoMetadata(json_data=json_data)
     else:
-        raise HTTPException(status_code=400, detail="Invalid file format. Only MP4 files are allowed.")
-            
+        raise HTTPException(
+            status_code=404, detail="Processed video and metadata not found."
+        )
+
+
+@app.post("/drone_footage")
+async def upload_drone_footage(file: UploadFile) -> tuple[FileResponse, VideoMetadata]:
+    if file.filename:
+        if not file.filename.endswith(".mp4"):
+            raise HTTPException(
+                status_code=400,
+                detail="Invalid file format. Only MP4 files are allowed.",
+            )
+        return get_video_from_path(file.filename)
+    else:
+        raise HTTPException(status_code=400, detail="No file found")
+
+
 @app.post("/grounding_dino")
-async def run_grounding_dino(query: str):
+async def run_grounding_dino(target_video: str, query: str):
+    file_response, _ = get_video_from_path(target_video)
 
+    video_path = file_response.path
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    video = decord.VideoReader(video_path)
+
+    results = []
+
+    for frame in video:
+        image = Image.fromarray(frame.asnumpy())
+
+        inputs = processor(images=image, text=query, return_tensors="pt").to(device)
+        with torch.no_grad():
+            outputs = model(**inputs)
+
+        frame_results = processor.post_process_grounded_object_detection(
+            outputs,
+            inputs.input_ids,
+            box_threshold=0.4,
+            text_threshold=0.3,
+            target_sizes=[image.size[::-1]]
+        )
+
+        results.append(frame_results)
+
+    return results
 
