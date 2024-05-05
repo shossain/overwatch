672605d1dcdc3643fb9bf587a03e24c64ead80dd
Justin W. Lin <justin@lin.ai>
2024-05-05 00:22:58 -0700

    feat: Add logging

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,24 @@
 import json
 import os
 
+import decord
 import torch
 from fastapi import FastAPI, HTTPException, UploadFile
 from fastapi.responses import FileResponse
 from PIL import Image
 from pydantic import BaseModel
 from transformers import AutoModelForZeroShotObjectDetection, AutoProcessor
-import decord
+import logging
 
 FILE_PATH = "files"
 app = FastAPI()
+logging.basicConfig(level=logging.INFO)
 
 model_id = "IDEA-Research/grounding-dino-base"
 device = "cuda"
 processor = AutoProcessor.from_pretrained(model_id)
 model = AutoModelForZeroShotObjectDetection.from_pretrained(model_id).to(device)
+
 
 
 class VideoMetadata(BaseModel):
@@ -24,50 +27,44 @@ class VideoMetadata(BaseModel):
 
 
 def get_video_from_path(video_name: str) -> VideoMetadata:
-    file_path = os.path.join(FILE_PATH, video_name)
-    if not os.path.exists(file_path):
-        raise HTTPException(status_code=404, detail="Video file not found.")
+    base_name = video_name.split(".")[0]
+    file_path = os.path.join(FILE_PATH, base_name, video_name)
+    json_file_path = os.path.join(FILE_PATH, base_name, f"{base_name}.json")
 
-    processed_folder = video_name.split(".")[0]
-    processed_video_path = os.path.join(FILE_PATH, processed_folder, video_name)
-    json_file_path = os.path.join(
-        FILE_PATH, processed_folder, f"{video_name.split('.')[0]}.json"
-    )
+    if not os.path.exists(file_path) or not os.path.exists(json_file_path):
+        raise HTTPException(status_code=404, detail="Video file or metadata not found.")
 
-    if os.path.exists(processed_video_path) and os.path.exists(json_file_path):
-        with open(json_file_path, "r") as json_file:
-            json_data = json.load(json_file)
+    with open(json_file_path, "r") as json_file:
+        json_data = json.load(json_file)
 
-        return VideoMetadata(file=processed_video_path, metadata=json_data)
-    else:
-        raise HTTPException(
-            status_code=404, detail="Processed video and metadata not found."
-        )
+    return VideoMetadata(file=file_path, metadata=json_data)
 
 
 @app.post("/drone_footage")
 async def upload_drone_footage(file: UploadFile) -> VideoMetadata:
+    logging.info(f"Received drone footage upload request: {file.filename}")
     if file.filename:
         if not file.filename.endswith(".mp4"):
+            logging.warning("Invalid file format attempted to be uploaded.")
             raise HTTPException(
                 status_code=400,
                 detail="Invalid file format. Only MP4 files are allowed.",
             )
-        return get_video_from_path(file.filename)
+        video = get_video_from_path(file.filename)
+        logging.info(f"Processed video for file: {file.filename}")
+        return video
     else:
+        logging.error("No file found in the request.")
         raise HTTPException(status_code=400, detail="No file found")
 
 
 @app.post("/grounding_dino")
 async def run_grounding_dino(target_video: str, query: str):
     video = get_video_from_path(target_video)
-
     video_path = video.file
-
+    logging.info(f"Running Grounding Dino on video path: {video_path}")
     video = decord.VideoReader(video_path)
-
     results = []
-
     for frame in video:
         image = Image.fromarray(frame.asnumpy())
 
@@ -80,10 +77,9 @@ async def run_grounding_dino(target_video: str, query: str):
             inputs.input_ids,
             box_threshold=0.4,
             text_threshold=0.3,
-            target_sizes=[image.size[::-1]]
+            target_sizes=[image.size[::-1]],
         )
 
         results.append(frame_results)
 
     return results
-
