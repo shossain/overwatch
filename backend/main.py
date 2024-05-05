069bbba2a118114886df99347875e1b1fbcc8c33
Justin W. Lin <justin@lin.ai>
2024-05-05 06:02:47 -0700

    feat: Refactor to return metadata and video

--- backend/main.py
+++ backend/main.py
@@ -4,16 +4,15 @@ import os
 from typing import List
 
 import cv2
-import decord
 import numpy as np
 import torch
 import uvicorn
-from fastapi import FastAPI, HTTPException, UploadFile
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
 from GroundingDINO.groundingdino.util.inference import (
     annotate,
     load_image_from_path,
-    load_model,
     predict,
 )
 from PIL import Image
@@ -21,90 +20,98 @@ from pydantic import BaseModel
 from tqdm import tqdm
 
 FILE_PATH = "files"
+
+
 app = FastAPI()
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 logging.basicConfig(level=logging.INFO)
 
 logging.info("Loading grounding-dino")
-try:
-    model = load_model(
-        "GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py",
-        "GroundingDINO/weights/groundingdino_swint_ogc.pth",
-    ).to("cuda")
-except Exception as e:
-    logging.error(f"Failed to load model: {e}")
-    raise
+# try:
+#     model = load_model(
+#         "GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py",
+#         "GroundingDINO/weights/groundingdino_swint_ogc.pth",
+#     ).to("cuda")
+# except Exception as e:
+#     logging.error(f"Failed to load model: {e}")
+#     raise
 
 
-class VideoMetadata(BaseModel):
-    file: FileResponse
-    metadata: List[List[dict]]
-
-
-def get_video_from_path(video_name: str) -> VideoMetadata:
+@app.get("/video/{video_name}")
+async def get_video(video_name: str) -> StreamingResponse:
     base_name = video_name.split(".")[0]
-    file_path = os.path.join(FILE_PATH, base_name, video_name)
-    json_file_path = os.path.join(FILE_PATH, base_name, f"{base_name}.json")
+    video_file_path = os.path.join(FILE_PATH, base_name, f"{base_name}_seg.mp4")
 
-    if not os.path.exists(file_path) or not os.path.exists(json_file_path):
-        raise HTTPException(status_code=404, detail="Video file or metadata not found.")
+    if not os.path.exists(video_file_path):
+        raise HTTPException(status_code=404, detail="Video file not found.")
 
-    with open(json_file_path, "r") as json_file:
-        json_data = json.load(json_file)
+    def iterfile():
+        with open(video_file_path, "rb") as file:
+            yield from file
 
-    file_response = FileResponse(file_path, media_type="video/mp4")
-    return VideoMetadata(file=file_response, metadata=json_data)
-
-
-@app.post("/drone_footage")
-async def upload_drone_footage(file: UploadFile) -> VideoMetadata:
-    logging.info(f"Received drone footage upload request: {file.filename}")
-    if file.filename:
-        if not file.filename.endswith(".mp4"):
-            logging.warning("Invalid file format attempted to be uploaded.")
-            raise HTTPException(
-                status_code=400,
-                detail="Invalid file format. Only MP4 files are allowed.",
-            )
-        video_data = get_video_from_path(file.filename)
-        logging.info(f"Processed video for file: {file.filename}")
-        return video_data
-    else:
-        logging.error("No file found in the request.")
-        raise HTTPException(status_code=400, detail="No file found")
+    return StreamingResponse(
+        iterfile(),
+        media_type="video/mp4",
+        headers={
+            "Content-Disposition": f"attachment;filename={base_name}_seg.mp4",
+        },
+    )
 
 
-@app.post("/grounding_dino")
-async def run_grounding_dino(target_video: str, query: str):
-    video = get_video_from_path(target_video)
-    video_path = video.file
-    logging.info(f"Running Grounding Dino on video path: {video_path}")
+@app.get("/metadata/{video_name}")
+async def get_metadata(video_name: str) -> List[str]:
+    base_name = video_name.split(".")[0]
+    metadata_file_path = os.path.join(
+        FILE_PATH, base_name, f"{base_name}_qualitative_log.json"
+    )
 
-    vr = decord.VideoReader(video_path)
-    frames = vr.get_batch(range(0, len(vr), 10)).asnumpy()
+    if not os.path.exists(metadata_file_path):
+        raise HTTPException(status_code=404, detail="Metadata file not found.")
 
-    results = []
-    for frame in tqdm(frames):
-        image = Image.fromarray(frame.astype(np.uint8))
-        output_path = "image.jpg"
-        image.save(output_path)
-        image_source, processed_image = load_image_from_path(output_path)
-        # image_source, processed_image = load_image(image)
+    with open(metadata_file_path, "r") as file:
+        metadata = json.load(file)
 
-        boxes, logits, phrases = predict(
-            model=model,
-            image=processed_image,
-            caption=query,
-            box_threshold=0.35,
-            text_threshold=0.25,
-        )
-
-        annotated_frame = annotate(
-            image_source=image_source, boxes=boxes, logits=logits, phrases=phrases
-        )
-        results.append(annotated_frame)
-
-    return results
+    return metadata
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+# @app.post("/grounding_dino")
+# async def run_grounding_dino(target_video: str, query: str):
+#     video = get_video_from_path(target_video)
+#     video_path = video.file
+#     logging.info(f"Running Grounding Dino on video path: {video_path}")
+
+#     vr = decord.VideoReader(video_path)
+#     frames = vr.get_batch(range(0, len(vr), 10)).asnumpy()
+
+#     results = []
+#     for frame in tqdm(frames):
+#         image = Image.fromarray(frame.astype(np.uint8))
+#         output_path = "image.jpg"
+#         image.save(output_path)
+#         image_source, processed_image = load_image_from_path(output_path)
+#         # image_source, processed_image = load_image(image)
+
+#         boxes, logits, phrases = predict(
+#             model=model,
+#             image=processed_image,
+#             caption=query,
+#             box_threshold=0.35,
+#             text_threshold=0.25,
+#         )
+
+#         annotated_frame = annotate(
+#             image_source=image_source, boxes=boxes, logits=logits, phrases=phrases
+#         )
+#         results.append(annotated_frame)
+
+#     return results
